3e72eab39ec9dc283ff15b2f290c90cd7f084089
Juha Törnroos <juha.tornroos@csc.fi>
2018-01-23 10:18:17 +0000

    Initial commit

---
+++ src/test/java/eu/elixir/permissionApi/PermissionApiApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.elixir.permissionApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PermissionApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
